0228419f13a6025a70cb252bbab84d8f64dc429a
Hoye Lam <hoyelam@protonmail.com>
2022-08-27 15:37:24 +0200

    Initial Commit

---
+++ TweetWithinAppExample/TweetWithinAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetWithinAppExampleApp.swift
+//  TweetWithinAppExample
+//
+//  Created by Hoye Lam on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TweetWithinAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
